2dd86adf53becf13ddeba9785d26c847aa286c30
Amna <amnaamir2310@gmail.com>
2025-05-26 01:49:47 +0300

    fixed api route

--- admin-api/src/routes/salesRoutes.js
+++ admin-api/src/routes/salesRoutes.js
@@ -5,7 +5,7 @@ const saleController = require('../controllers/saleController');
 // Routes for api endpoints
 router.get('/getSales', saleController.getAllSales);    // Get all sales
 router.get('/filterSales', saleController.filterSales); // Get Filter sales
-router.get('/DailyRevenue', saleController.getDailyRevenue);    // Get Revenue Grouped Daily
+router.get('/dailyRevenue', saleController.getDailyRevenue);    // Get Revenue Grouped Daily
 router.get('/weeklyRevenue', saleController.getWeeklyRevenue);  // Get Revenue Grouped Weekly
 router.get('/monthlyRevenue', saleController.getMonthlyRevenue);    // Get Revenue Grouped Monthly
 router.get('/annualRevenue', saleController.getAnnualRevenue);  // Get Revenue Grouped Annually
